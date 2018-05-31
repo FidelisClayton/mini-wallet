b864c99ce3df1d7445f77600efa79171476310cf
Clayton Fidelis <clayton.fidelis@live.com>
2018-05-31 10:01:55 -0300

    refactor(colors): add color helpers

---
+++ src/helpers/colors.js
@@ -0,0 +1,12 @@
+export const darkBlue = '#344760'
+export const lightBlue = '#7a95b9'
+
+export const lightGreen = '#8BC34A'
+export const darkGreen = '#4CAF50'
+export const inactiveLightGreen = 'rgba(76, 175, 80, 0.5)'
+
+export const lightGrey = '#EdEFF3'
+export const darkGrey = '#4E4E4E'
+
+export const cardShadow = '0px 0px 20px 0px rgba(0,0,0,0.3)'
+export const cardBorder = '1px solid #d8e0ea'
