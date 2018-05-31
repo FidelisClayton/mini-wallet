85f58e51f57060a6d75ec8b926c9b16e056575f1
Clayton Fidelis <clayton.fidelis@live.com>
2018-05-30 21:44:49 -0300

    feat(home): add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { css } from 'emotion'
+
+const styles = css({
+  '.nav': {
+    '&__menu': {
+      listStyleType: 'none',
+      display: 'flex',
+      justifyContent: 'space-around',
+      padding: 0
+    },
+
+    '&__item': {
+      textAlign: 'center',
+      marginLeft: '15px',
+      marginRight: '15px',
+      textTransform: 'uppercase',
+    },
+
+    '&__link': {
+      textDecoration: 'none',
+      color: '#344760',
+      fontWeight: 'bold',
+      paddingBottom: '8px',
+      display: 'block',
+      position: 'relative',
+      transition: '300ms color',
+
+      '&::after': {
+        content: '\'\'',
+        position: 'absolute',
+        height: '2px',
+        width: 0,
+        backgroundColor: '#4CAF50',
+        left: 0,
+        bottom: 0,
+        transition: '300ms ease-in-out width'
+      },
+
+      '&--active': {
+        color: '#4CAF50',
+
+        '&::after': {
+          width: '100%'
+        }
+      }
+    }
+  }
+})
+
+const Navbar = props => {
+  return (
+    <nav className={`nav ${styles}`}>
+      <ul className="nav__menu">
+        <li className="nav__item">
+          <Link
+            to="/"
+            className="nav__link nav__link--active"
+          >
+            Moedas
+          </Link>
+        </li>
+        <li className="nav__item">
+          <Link
+            to="/"
+            className="nav__link"
+          >
+            Transações
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
