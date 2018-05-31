65b456aa5bfb0717e9a31012e884614ba3561be2
Clayton Fidelis <clayton.fidelis@live.com>
2018-05-31 09:52:39 -0300

    feat(transactions): update transactions page to display transaction components

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import Transaction from './Transaction'
+
 const Transactions = (props) => {
   return (
-    <h1>Transactions</h1>
+    <div className="transactions">
+      <Transaction
+        fromCoin="BRL"
+        fromAmount={500}
+        toCoin="BRT"
+        toAmount={134.40}
+        toPrice={3.72}
+        date={new Date()}
+      />
+
+      <Transaction
+        fromCoin="BRL"
+        fromAmount={500}
+        toCoin="BTC"
+        toAmount={0.00700000}
+        toPrice={3.72}
+        date={new Date()}
+      />
+    </div>
   )
 }
 
