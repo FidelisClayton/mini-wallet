977526fcef54160af033078a09caee0dbb945d1c
Clayton Fidelis <clayton.fidelis@live.com>
2018-06-01 10:45:36 -0300

    feat(balance): add initial data for user wallets

--- src/database/user.js
+++ src/database/user.js
@@ -1,8 +1,34 @@
 import db from './index'
 
+const initialBalance = 100000
+
 export const createUser = (user) => {
   return db.put({
     _id: user.email,
+    wallets: [
+      {
+        id: 'brl',
+        token: 'BRL',
+        name: 'Real',
+        balance: initialBalance,
+        decimalPlaces: 2
+      },
+      {
+        id: 'btc',
+        token: 'BTC',
+        name: 'Bitcoin',
+        balance: 0,
+        decimalPlaces: 8
+      },
+      {
+        id: 'brt',
+        token: 'BRT',
+        name: 'Brita',
+        balance: 0,
+        decimalPlaces: 2
+      }
+    ],
+    transactions: [],
     ...user
   })
 }
